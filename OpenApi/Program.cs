4f3d206f3dc2854f4f7f3cef56648b71c0238494
Maksim Ivanyuk <ivanyuk.maksim@gmail.com>
2021-10-27 11:34:25 +0300

    Initial commit

---
+++ OpenApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenApi.PetClient;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace OpenApi
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection()
+                .Configure<PetClientOptions>(o => { })
+                .BuildServiceProvider();
+            
+            var client = new PetStoreClient(services.GetRequiredService<IOptionsSnapshot<PetClientOptions>>(), new System.Net.Http.HttpClient());
+            await client.AddPetAsync(new Pet
+            {
+                Category = new Category() { Id = 1, Name = "asdfasdf" },
+                Name = "asdfasdf"
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
